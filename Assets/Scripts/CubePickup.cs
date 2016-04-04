adb8348e86bb0fb90bcf5dcd6070e5856fb9176a
Megawats777 <jedsemilla@gmail.com>
2016-04-04 17:17:30 -0400

    Addedpickup class

---
+++ Assets/Scripts/CubePickup.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubePickup : MonoBehaviour
+{
+    /*----Variables----*/
+
+    // The rate at which the cube will rotate
+    public float rotationRate = 15.0f;
+
+    // Reference to the box collider
+    BoxCollider collisionBox;
+
+    // Reference to the mesh renderer 
+    MeshRenderer cubeMesh;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Get the box collider
+        collisionBox = GetComponent<BoxCollider>();
+
+        // Get the mesh renderer
+        cubeMesh = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Rotate the sphere
+        rotateCube();
+    }
+
+    // Rotate the sphere
+    private void rotateCube()
+    {
+        transform.Rotate(new Vector3(rotationRate * Time.deltaTime, rotationRate * Time.deltaTime, rotationRate * Time.deltaTime));
+    }
+
+    // Disable the cube
+    private void disableCube()
+    {
+        // Disable the box collider
+        collisionBox.enabled = false;
+
+        // Disable the mesh renderer
+        cubeMesh.enabled = false;
+    }
+
+}
