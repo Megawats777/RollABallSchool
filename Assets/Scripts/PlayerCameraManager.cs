8c15b364c660c36fa75f4e6a9d131cb3f84cba14
Megawats777 <jedsemilla@gmail.com>
2016-04-04 17:00:49 -0400

    Added functionality for the player camera to track the player sphere

--- Assets/Scripts/PlayerCameraManager.cs
+++ Assets/Scripts/PlayerCameraManager.cs
@@ -1,15 +1,50 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerCameraManager : MonoBehaviour {
+public class PlayerCameraManager : MonoBehaviour
+{
+    /*----Variables----*/
 
-	// Use this for initialization
-	void Start () {
-	
+    // Reference to the player sphere
+    PlayerSphere playerSphereRef;
+
+    // The new camera location
+    Vector3 cameraLocation = new Vector3(0.0f, 0.0f, 0.0f);
+
+    // The camera distance on the Z axis
+    public float cameraDistanceZ = 20.0f;
+
+    // The camera distance on the Y axis
+    public float cameraDistanceY = 17.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+        // Get the PlayerSphere
+        playerSphereRef = FindObjectOfType<PlayerSphere>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        // Set the camera's new location
+        setCameraLocation();
 	}
+
+    // Set the camera's new location
+    private void setCameraLocation()
+    {
+        // The new camera location on the Z axis
+        float newLocationZ = 0.0f;
+
+        newLocationZ = playerSphereRef.transform.position.z - cameraDistanceZ;
+
+        // Set the new camera location
+        cameraLocation = new Vector3(playerSphereRef.transform.position.x, cameraDistanceY, newLocationZ);
+
+        // Set the transform position to the new camera location
+        transform.position = cameraLocation;
+
+    }
+
 }
--- Assets/Scripts/PlayerSphere.cs
+++ Assets/Scripts/PlayerSphere.cs
@@ -31,7 +31,7 @@ public class PlayerSphere : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         // Perform the sphere's movement    
         sphereMovement();
